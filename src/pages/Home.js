9af084eb2384ea8baeb360e68d267d0a3b2d99b8
jodiezhuu <jodiezhu12@gmail.com>
2022-08-30 17:14:33 -0400

    page load animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { useNav } from '../customHooks/useNav';
-
+import './Home.css'
 export default function Home() {
 	const homeRef = useNav('Home');
-
+	const imgBackground = "/imgs/homescreenbackground.png";
+	const logoBackground = '/imgs/jodielogo.png'
 	return (
+		<>
 		<section ref={homeRef} id='homeContainer'>
-			<div>
-				<h3>home</h3>
-				<p>This is the home section</p>
-			</div>
+			<img className = 'homeBackground' src={process.env.PUBLIC_URL + imgBackground}></img>
+			<h1 className = 'header'>2A Computer Science Student at the University of Waterloo</h1>
+			<img className = 'logo' src={process.env.PUBLIC_URL + logoBackground}></img>
+			<p className = 'header'>Seeking Summer 2023 Internships</p>
 		</section>
+		</>
 	);
 }
