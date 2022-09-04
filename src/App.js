df0277579ffbb9f6ff9e6d3c7791d263a2d19bf2
jodiezhuu <jodiezhu12@gmail.com>
2022-09-03 21:50:25 -0400

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react';
-import About from './pages/About';
-import Home from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
 import NavProvider from './context/NavContext';
 import { Main } from './pages';
 import { Nav } from './nav';
