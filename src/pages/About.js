cf740b42c9c8088d15c3bc9f654d999f21c7b7e7
jodiezhuu <jodiezhu12@gmail.com>
2022-09-17 01:47:33 -0400

    update about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,12 +11,21 @@ export default function About() {
 		<section ref={aboutRef} id='aboutContainer'>
 			<h1 className = 'aboutTitle'>ABOUT</h1>
 				<p className = 'description'>
-					My interests focus mainly around software development,
-					music, and exercising. Particularly, I'm interested in full
-					stack development and data engineering. On the contrary, 
-					in order to balance my stress, I enjoy practicing mental
-					wellness disciplines such as listening to 
-					tunes and going to spin class!
+					Hi everyone! My name is Jodie and I'm a Computer Science student at the 
+					University of Waterloo. I've previously interned at MedMe
+					Health and adnog technology as a software engineer, working 
+					in agile environments, building backends, features, and infrastructure. 
+				<br />
+				<br />
+					I am a curious learner and I love to apply my critical and
+					problem-solving skills to coding! Specifically, I enjoy full-stack
+					development and data science with technologies including Python,
+					React, and Java.
+				<br />
+				<br />
+					Outside of work, my interests focus mainly around
+					music and physical activity! Particularly, I love 
+					creating seasonal Spotify playlists and going to spin class!
 				</p>
 			<a href='https://open.spotify.com/user/itsmejodie_12?si=4c8d72843f844994' target='_blank'>
 				<BsSpotify className = 'spotifyIcon'/>
