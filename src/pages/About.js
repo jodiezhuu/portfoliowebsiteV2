090693ca8a37cd192c2eda70b56968973e5aa2a9
jodiezhuu <jodiezhu12@gmail.com>
2023-07-25 15:16:45 -0400

    chore: update description

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,20 +12,18 @@ export default function About() {
 			<h1 className = 'aboutTitle'>ABOUT</h1>
 				<p className = 'description'>
 					Hi everyone! My name is Jodie and I'm a Computer Science student at the 
-					University of Waterloo. I've previously interned at MedMe
-					Health and adnog technology as a software engineer, working 
+					University of Waterloo. I've previously interned at Tech Matters, MedMe
+					Health, and adnog technology as a software engineer, working 
 					in agile environments, building backends, features, and infrastructure. 
 				<br />
 				<br />
-					I am a curious learner and I love to apply my critical thinking and
-					problem-solving skills to coding! Specifically, I enjoy full-stack
-					development and data science with technologies including Python,
-					React, and Java.
+					I enjoy mind puzzles and translating these skills to my personal side projects and
+					in the professional workplace. Specifically, I have experience with full-stack
+					and iOS development, specializing in frameworks like React and Django.
 				<br />
 				<br />
-					Outside of work, my interests focus mainly around
-					music and physical activity! Particularly, I love 
-					creating seasonal Spotify playlists and going to spin class!
+					In my off time, I like to stay physically active. Particularly, I'm an 
+					amateur golfer and a spin addict!
 				</p>
 			<a href='https://open.spotify.com/user/itsmejodie_12?si=4c8d72843f844994' target='_blank'>
 				<BsSpotify className = 'spotifyIcon'/>
