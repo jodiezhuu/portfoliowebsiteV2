bdedca179c40067eeeace86a0bb2f99536964055
jzhuwaterloo <jodie.zhu@uwaterloo.ca>
2024-10-22 14:06:50 -0400

    updaet link

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -64,7 +64,7 @@ export default function Projects() {
               </Card>
               </a>
               </div>
-              <a href='https://drive.google.com/file/d/1xAx7PQ0YvCo3_zMJ61UuEXVFFmARpf5B/view?usp=sharing' target = '_blank'>
+              <a href='https://drive.google.com/file/d/1g9vV4pC9QYgl3d1QrCxy78JUI1fhXzOm/view?usp=sharing' target = '_blank'>
               <button className = 'resumeButton'>DOWNLOAD CV</button>
               </a>
         </section>
